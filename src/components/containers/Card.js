c20c9de587fa82e95fa090e7e1f6b5d855c52863
sebastian4260 <br7cervantes@gmail.com>
2021-03-06 12:02:25 -0500

    Edit card.js to use for favorites card

--- src/components/containers/Card.js
+++ src/components/containers/Card.js
@@ -1,21 +1,26 @@
 import styled from "@emotion/styled";
 
 const sizes = {
-  default: { width: "140px" },
-  small: { width: "106px" },
+  default: { width: "140px" , height: "140px" },
+  small: { width: "300px" , height:"56px" },
+  
 };
 
 const StyledCard = styled.div`
   width: ${(props) =>
     sizes[props.size] ? sizes[props.size].width : sizes.default.width};
+  height: ${(props) =>
+    sizes[props.size] ? sizes[props.size].height : sizes.default.height};  
   background: #ffffff;
   box-shadow: 2px 2px 0px rgba(0, 0, 0, 0.25);
   border-radius: 4px;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
+  justify-content: space-around;
   padding: 13px;
 `;
+
 
 function Card({ children, size }) {
   return <StyledCard size={size}>{children}</StyledCard>;
