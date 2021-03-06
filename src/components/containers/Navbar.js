7d9d6825e1f6dfbddfaeafe739cafbef447b8033
sebastian4260 <br7cervantes@gmail.com>
2021-03-06 12:36:11 -0500

    Delete margin in navbar

--- src/components/containers/Navbar.js
+++ src/components/containers/Navbar.js
@@ -10,7 +10,7 @@ const StyledDiv = styled.div`
   justify-content: center;
   background: #f2f2f2;
   box-shadow: 0px -2px 0px rgba(0, 0, 0, 0.25);
-  margin-top: 20px;
+  
   .icons-container {
     width: 250px;
     height: 50px;
